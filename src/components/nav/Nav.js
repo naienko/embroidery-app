9f57afe895706afcdeb951659e3da94920688615
Panya Farnette <naienko@gmail.com>
2019-10-25 16:11:37 -0500

    bar collapses and toggles for phone

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -5,28 +5,39 @@ import { withRouter } from "react-router";
 import { Navbar, Nav, NavItem, NavLink, NavbarToggler, Collapse } from "reactstrap";
 
 class NavBar extends Component {
+	state ={
+		collapse: false
+	}
+
     logout = () => {
-        APIManager.logout()
-    }
-    
+		APIManager.logout();
+		localStorage.clear();
+		sessionStorage.clear();
+	}
+	
+	toggle = () => this.setState({collapse: !this.state.collapse});
+	
     render() {
         return (
-            <Navbar expand="md" color="light" light id="nav">
-            <Nav navbar>
-                <NavItem>
-                    <NavLink href="/">Home</NavLink>
-                </NavItem>
-                <NavItem>
-                    Welcome <a href="/profile">{this.props.activeUser.username}</a>
-                </NavItem>
-				<NavItem>
-                    <NavLink href="/stash/new">Add New Stack</NavLink>
-                </NavItem>
-                <NavItem>
-                    <NavLink onClick={this.logout}>Logout</NavLink>
-                </NavItem>
-			</Nav>
-            </Navbar>
+			<Navbar light expand="md" color="light" className="sticky-top">
+				<Nav pills fill className="w-100">
+					<NavItem className="m-1">
+						<NavLink href="/" active>Home</NavLink>
+					</NavItem>
+					<NavItem className="m-1 pt-2">
+						Welcome <a href="/profile">{this.props.activeUser.username}</a>
+					</NavItem>
+					<NavItem className="m-1">
+						<NavLink href="/stash/new">Add New Stash</NavLink>
+					</NavItem>
+					<NavbarToggler onClick={this.toggle} />
+					<Collapse isOpen={this.state.collapse}>
+						<NavItem className="m-1">
+							<NavLink onClick={this.logout}>Logout</NavLink>
+						</NavItem>
+					</Collapse>
+				</Nav>
+			</Navbar>
         )
     }
 }
